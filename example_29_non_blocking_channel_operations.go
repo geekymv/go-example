2b680158325e875ed1096f300a1470bb5013e6e7
geekymv <ym2011678@foxmail.com>
2021-12-18 00:16:08 +0800

    non blocking channel operations

---
+++ example_29_non_blocking_channel_operations.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+使用select default 语句实现非阻塞发送、接收
+*/
+func main() {
+
+	// 没有缓冲的channel
+	messages := make(chan string)
+
+	// 非阻塞接收
+	select {
+	case msg := <-messages:
+		fmt.Println("received message:", msg)
+	default:
+		fmt.Println("no message received")
+	}
+
+	// 非阻塞发送
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("sent message", msg)
+	default:
+		fmt.Println("no message sent")
+	}
+
+
+	signals := make(chan bool)
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+
+	}
+
+}
