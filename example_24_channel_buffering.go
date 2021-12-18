efb4ad04fbf59445485d0499f461ee99c1e3fb96
geekymv <ym2011678@foxmail.com>
2021-12-18 23:45:23 +0800

    channel buffer

--- example_24_channel_buffering.go
+++ example_24_channel_buffering.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+/*
+带缓冲的 channel
+当 channel 已满，再向里面写数据，就会阻塞
+当 channel 已空，从里面取数据，也会阻塞
+*/
 func main() {
 
 	// 带缓冲的channel，缓冲2个值
 	messages := make(chan string, 2)
-	messages <- "ping"
-	messages <- "hello"
 
-	fmt.Println(<-messages)
-	fmt.Println(<-messages)
+	go func() {
+		defer fmt.Println("child goroutine exit")
+
+		for i := 0; i < 3; i++ {
+			messages <- fmt.Sprintf("%s%d", "hello-", i)
+			fmt.Println("message len is ", len(messages), ", cap is ", cap(messages))
+		}
+	}()
+
+	time.Sleep(2 * time.Second)
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(<- messages)
+	}
+
+	fmt.Println("main exit")
+
 }
