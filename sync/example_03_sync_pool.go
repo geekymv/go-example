136926213f62535c29b28078b2bdba52e7450d12
geekymv <ym2011678@foxmail.com>
2022-04-11 17:49:04 +0800

    sync pool

---
+++ sync/example_03_sync_pool.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+// https://cloud.tencent.com/developer/article/1847917
+func main() {
+
+	var count int64
+
+	// 无法指定pool里面可以存储的元素个数
+	pool := sync.Pool{
+		New: func() interface{} {
+			atomic.AddInt64(&count, 1)
+			fmt.Println("return")
+			return make([]int, 5)
+		},
+	}
+
+	var wg sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			p := pool.Get()
+			fmt.Printf("%v\n", &p)
+			pool.Put(p)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Println(count)
+
+}
