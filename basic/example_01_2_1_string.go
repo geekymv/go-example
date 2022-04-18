f8dc274bd9faf5cbb6029d2b2d33b4cd12601fb0
geekymv <ym2011678@foxmail.com>
2022-04-18 15:53:29 +0800

    strings Count

--- basic/example_01_2_1_string.go
+++ basic/example_01_2_1_string.go
@@ -13,4 +13,10 @@ func main() {
 
 	strs := strings.Split("", ",")
 	fmt.Println(strs)
+
+	// Count 统计
+	//str2 := `["qwww","sgxzzzzz"]`
+	str2 := `[6]`
+	count := strings.Count(str2, ",")
+	fmt.Println("count =", count)
 }
