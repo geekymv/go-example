7a9fc034aeab508142a4a4160bd45acd89e9a303
geekymv <ym2011678@foxmail.com>
2022-04-21 18:02:02 +0800

    byte

--- basic/example_01_4_byte.go
+++ basic/example_01_4_byte.go
@@ -8,4 +8,9 @@ import (
 func main() {
 	split := bytes.Split([]byte("hello\nworld\nhaha"), []byte("\n"))
 	fmt.Println(split)
+
+	b1 := []byte("a b c d e")
+	// [][]byte
+	b2 := bytes.Split(b1, []byte(" "))
+	fmt.Println(b2[0])
 }
