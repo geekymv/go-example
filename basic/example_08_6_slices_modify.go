ec44713e9392719a7e2276ec91af7345ce3d6b8e
geekymv <ym2011678@foxmail.com>
2022-04-21 19:06:27 +0800

    modify slice struct field

--- basic/example_08_6_slices_modify.go
+++ basic/example_08_6_slices_modify.go
@@ -8,16 +8,24 @@ type UserInfo struct {
 }
 
 func main() {
-	var userList []UserInfo
-	userList = append(userList, UserInfo{
+	userList := make([]UserInfo, 0, 2)
+	fmt.Printf("len:%v, cap:%v\n", len(userList), cap(userList))
+
+	user1 := UserInfo{
 		Name: "zhangsan",
 		Age:  22,
-	})
-
-	userList = append(userList, UserInfo{
+	}
+	user2 := UserInfo{
 		Name: "lisi",
 		Age:  20,
-	})
+	}
+
+	// append value copy
+	userList = append(userList, user1, user2)
+
+	// 不是一个地址
+	fmt.Printf("user1 addr:%p\n", &user1)
+	fmt.Printf("user1 addr:%p\n", &userList[0])
 
 	// 用户信息没有改掉
 	for _, user := range userList {
@@ -34,4 +42,17 @@ func main() {
 		fmt.Printf("index %p\n", &userList[i])
 	}
 	fmt.Printf("%v\n", userList)
+
+	fmt.Println("------------------------------------")
+
+	var userList2 []*UserInfo
+	fmt.Println(user1.Age)
+	userList2 = append(userList2, &user1, &user2)
+	for _, user := range userList2 {
+		user.Age = user.Age + 2
+	}
+	for _, user := range userList2 {
+		fmt.Println(*user)
+	}
+
 }
